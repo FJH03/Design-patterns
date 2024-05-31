6b34ba6c0871892acfca3a00e4d8f611755f8ded
FJH_03 <fengjiahe5@gmail.com>
2024-05-31 17:53:55 +0800

    原型模式

---
+++ src/main/java/com/example/prototype/Prototype.java
@@ -0,0 +1,30 @@
+package com.example.prototype;
+
+/**
+ * @description 原型模式（通过clone方法创建的Prototype对象不会执行构造方法）
+ * @author: muse
+ */
+public class Prototype implements Cloneable {
+    public static void main(String[] args) {
+        System.out.println("【步骤1】开始执行Prototype类型的对象创建工作");
+        Prototype prototype = new Prototype();
+
+        /** 执行clone方法创建的Prototype对象 */
+        System.out.println("【步骤2】开始执行clone操作");
+        Prototype prototype1 = prototype.clone();
+    }
+
+    public Prototype() {
+        System.out.println("-----Prototype的构造方法被执行了！-----");
+    }
+
+    @Override
+    protected Prototype clone() {
+        try {
+            return (Prototype) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/example/prototype/Prototype1.java
@@ -0,0 +1,68 @@
+package com.example.prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+/**
+ * @description 原型模式（浅拷贝&深拷贝）
+ * @author: muse
+ */
+@Data
+public class Prototype1 implements Cloneable {
+    private String name;
+    private List<String> arrayList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Prototype1 prototype1 = new Prototype1();
+        prototype1.setName("original object");
+        prototype1.setValue("original object");
+
+        Prototype1 clonePrototype1 = prototype1.clone();
+        clonePrototype1.setName("clone object");
+        /** 发现添加了执行了clone对象的setValue之后，也修改了prototype1中的arrayList中数据 */
+        clonePrototype1.setValue("clone object");
+        System.out.println("prototype1=" + prototype1);
+        System.out.println("clonePrototype1=" + clonePrototype1);
+    }
+
+    public void setValue(String value) {
+        this.arrayList.add(value);
+    }
+
+    public List<String> getValue() {
+        return this.arrayList;
+    }
+
+    /**
+     * 浅拷贝
+     * @return
+     */
+    @Override
+    protected Prototype1 clone() {
+        try {
+            return (Prototype1) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 深拷贝
+     * @return
+     */
+//    @Override
+//    protected Prototype1 clone() {
+//        Prototype1 prototype1 = null;
+//        try {
+//            prototype1 = (Prototype1) super.clone();
+//            prototype1.setArrayList(new ArrayList<>(prototype1.getArrayList()));
+//        } catch (CloneNotSupportedException e) {
+//            e.printStackTrace();
+//        }
+//        return prototype1;
+//    }
+
+}
